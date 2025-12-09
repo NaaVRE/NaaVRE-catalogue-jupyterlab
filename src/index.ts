d2abd5138cdf0c72f052ce1fadbfdc67589308d8
Gabriel Pelouze <gabriel@pelouze.net>
2025-12-09 11:04:06 +0100

    fix code style

--- src/index.ts
+++ src/index.ts
@@ -13,17 +13,28 @@ const plugin: JupyterFrontEndPlugin<void> = {
   description: 'NaaVRE assets catalogue frontend on Jupyter Lab',
   autoStart: true,
   optional: [ISettingRegistry],
-  activate: (app: JupyterFrontEnd, settingRegistry: ISettingRegistry | null) => {
-    console.log('JupyterLab extension @naavre/catalogue-jupyterlab is activated!');
+  activate: (
+    app: JupyterFrontEnd,
+    settingRegistry: ISettingRegistry | null
+  ) => {
+    console.log(
+      'JupyterLab extension @naavre/catalogue-jupyterlab is activated!'
+    );
 
     if (settingRegistry) {
       settingRegistry
         .load(plugin.id)
         .then(settings => {
-          console.log('@naavre/catalogue-jupyterlab settings loaded:', settings.composite);
+          console.log(
+            '@naavre/catalogue-jupyterlab settings loaded:',
+            settings.composite
+          );
         })
         .catch(reason => {
-          console.error('Failed to load settings for @naavre/catalogue-jupyterlab.', reason);
+          console.error(
+            'Failed to load settings for @naavre/catalogue-jupyterlab.',
+            reason
+          );
         });
     }
   }
