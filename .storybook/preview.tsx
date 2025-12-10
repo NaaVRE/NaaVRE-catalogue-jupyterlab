99e8f48daf268af2f5408869ac752b28015d24f0
Gabriel Pelouze <gabriel@pelouze.net>
2025-12-10 17:01:54 +0100

    fix lint errors

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -7,29 +7,29 @@ const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
-      },
+        color: /(background|color)$/i,
+        date: /Date$/i
+      }
     },
     settings: {
       virtualLab: 'test-virtual-lab-1',
       catalogueServiceUrl: 'http://localhost:8000'
-    },
+    }
   },
   decorators: [
     (Story, { parameters }) => {
       return (
         <div
           style={{
-        fontFamily:
-          "system-ui,-apple-system,blinkmacsystemfont,'Segoe UI',helvetica,arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol'"
-      }}
-    >
-      <SettingsContext.Provider value={parameters.settings}>
-        <Story />
-        </SettingsContext.Provider>
+            fontFamily:
+              "system-ui,-apple-system,blinkmacsystemfont,'Segoe UI',helvetica,arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol'"
+          }}
+        >
+          <SettingsContext.Provider value={parameters.settings}>
+            <Story />
+          </SettingsContext.Provider>
         </div>
-    );
+      );
     }
   ]
 };
